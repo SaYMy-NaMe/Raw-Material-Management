75a765e270a736ca445558486723c16b275feb1e
ashraf <ashrafurrahman42069@gmail.com>
2023-10-30 17:31:55 +0600

    change password functionality implementation

--- src/pages/change-password/ChangePassword.jsx
+++ src/pages/change-password/ChangePassword.jsx
@@ -1,14 +1,39 @@
+import { toast } from "react-hot-toast";
 import InputField from "../../components/InputField";
 import "./changePassword.css";
+import { useNavigate } from "react-router-dom";
+
 const ChangePassword = () => {
+  const navigate = useNavigate();
+  const accessToken = "";
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const changePasswordData = {
       oldPassword: e.target.oldPassword.value,
       newPassword: e.target.newPassword.value,
     };
-    console.log(changePasswordData);
+    // Make a POST request to the change Password API
+    fetch("https://icsrmms.vercel.app/changePassword", {
+      method: "POST",
+      headers: {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(changePasswordData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "password Updated successfully") {
+          toast.success("Your password has been updated!");
+          navigate("/");
+        } else {
+          toast.error(data.message);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred during Sign in. Please try again.");
+      });
   };
   return (
     <div id="changePassword">
