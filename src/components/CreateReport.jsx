51c3f98120e0df11ef70b470bc7ba0e7446d0cd9
ashraf <ashrafurrahman42069@gmail.com>
2023-11-10 17:20:02 +0600

    Create Report Done

---
+++ src/components/CreateReport.jsx
@@ -0,0 +1,24 @@
+import InputField from "./InputField";
+
+const CreateReport = () => {
+    return (
+        <div id="createReport" action="submit">
+            <form>
+            <InputField
+          type="date"
+          name="start_date"
+          fieldName="startDate"
+          placeholder="Start Date"
+        />
+        <InputField
+          type="date"
+          name="end_date"
+          fieldName="endDate"
+          placeholder="End Date"
+        />
+            </form>
+        </div>
+    );
+};
+
+export default CreateReport;
